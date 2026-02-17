14635b9724c3f65a89554f76a0c619a01e921564
mitchxlt <mitch at unreif dot eu>
2026-02-17 09:24:02 +0100

    Add failing test for "flask" with elsewhere-known d

--- tests.sh
+++ tests.sh
@@ -2,4 +2,6 @@
 ./solver -d ngerman -r ms . ^e ^i . ^t | grep -i ratio && echo "ratio: Test failed (missing the elsewhere-e)"
 ./solver -d american-english -r nef ^s ^r o s t | grep -i ghost && echo "ghost: Test failed (missing the elsewhere-r)"
 ./solver -d ngerman  -r ten ^s . . ^i . | grep -i abgas && echo "abgas: Test failed (missing the elsewhere-i)"
+./solver -d american-english -r ychtzebrio ^a ^ad . . ^as | grep -i flask && echo "flask: Test failed (missing the elsewhere-d)"
 ./solver -d ngerman -r tenko ^s i ^f ^is ^s | grep -i fisch || echo "fisch: Test failed (is the solution)"
+
