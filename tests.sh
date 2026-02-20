75d7fa3ec92432b275409878a9924a5ca66c4c0f
mitchxlt <mitch@unreif.eu>
2026-02-20 08:14:16 +0000

    Renamed dictionaries

--- tests.sh
+++ tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-./solver -d ngerman -r ms . ^e ^i . ^t | grep -i ratio && echo "ratio: Test failed (missing the elsewhere-e)"
-./solver -d american-english -r nef ^s ^r o s t | grep -i ghost && echo "ghost: Test failed (missing the elsewhere-r)"
-./solver -d ngerman  -r ten ^s . . ^i . | grep -i abgas && echo "abgas: Test failed (missing the elsewhere-i)"
-./solver -d american-english -r ychtzebrio ^a ^ad . . ^as | grep -i flask && echo "flask: Test failed (missing the elsewhere-d)"
-./solver -d ngerman -r tenko ^s i ^f ^is ^s | grep -i fisch || echo "fisch: Test failed (is the solution)"
+./solver -d de -r ms . ^e ^i . ^t | grep -i ratio && echo "ratio: Test failed (missing the elsewhere-e)"
+./solver -d us -r nef ^s ^r o s t | grep -i ghost && echo "ghost: Test failed (missing the elsewhere-r)"
+./solver -d de  -r ten ^s . . ^i . | grep -i abgas && echo "abgas: Test failed (missing the elsewhere-i)"
+./solver -d us -r ychtzebrio ^a ^ad . . ^as | grep -i flask && echo "flask: Test failed (missing the elsewhere-d)"
+./solver -d de -r tenko ^s i ^f ^is ^s | grep -i fisch || echo "fisch: Test failed (is the solution)"
 
